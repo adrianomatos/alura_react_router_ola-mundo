927f289cae8fb8025edf07d273397ab2386d2d06
Adriano Matos <adrianotaua@hotmail.com>
2024-07-13 10:22:42 -0300

    Ex básico sobre rotas com JS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Inicio from "./Paginas/Inicio";
+import SobreMim from "./Paginas/SobreMim";
+
+// Ex básico sobre rotas com JS
+const pagina = window.location.pathname === "/" ? <Inicio /> : <SobreMim />;
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Olá Mundo!</h1>
-    </div>
-  );
+  return pagina;
 }
 
 export default App;
